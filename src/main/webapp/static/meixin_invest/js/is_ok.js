e3a1263b52c12069bf7f951d9b00c0d0bae0cd0a
jingyu.zhang@meixinfinance.com <jingyu.zhang@meixinfinance.com>
2016-08-08 14:09:00 +0800

    接口分离

--- src/main/webapp/static/meixin_invest/js/is_ok.js
+++ src/main/webapp/static/meixin_invest/js/is_ok.js
@@ -3,13 +3,18 @@
  */
 var ajax_ok = false;
 var token = $.cookie('mx_sid');
+
 if (!token) {
-    //window.location = 'https://www.meixinfinance.com';
+    window.location = 'https://www.meixinfinance.com';
 }
+
+//设置遮罩层高度
 function setHeight() {
     $('.ajax_wait').height($(window).height());
 }
+
 $(function () {
+    //页面加载完成,get请求
     var getAjaxData = function (url, type) {
         $.ajax({
             type: 'get',
@@ -17,7 +22,6 @@ $(function () {
             data: null,
             timeout: 5e3,
             success: function (data) {
-                console.log(JSON.stringify(data));
                 if (data && data.code && data.code == -1) {
                     //window.location = 'https://www.meixinfinance.com/login';
                 }
@@ -44,6 +48,43 @@ $(function () {
             }
         });
     };
+    var getAjaxData = function (url, data, type) {
+        $.ajax({
+            type: 'post',
+            url: url,
+            data: data,
+            timeout: 5e3,
+            success: function (data) {
+                if (data && data.code && data.code == -1) {
+                    //window.location = 'https://www.meixinfinance.com/login';
+                    login();
+                    console.log(is_login);
+                }
+
+                if (data && data.code && data.code == 0) {
+                    // 提示网络问题
+                }
+
+                if (data && data.code && data.code == 1) {
+                    switch (type) {
+                        case "login":
+                            login();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            },
+            complete: function (XMLHttpRequest, status) { //请求完成后最终执行参数
+                if (status == 'timeout') {//超时,status还有success,error等值的情况
+                    // ajaxTimeoutTest.abort();
+                    //提示网络问题
+                }
+            }
+        });
+    };
+
+
     getAjaxData('http://101.201.112.171:8082/apiv2/is_login', 'login');
 
 
@@ -52,6 +93,6 @@ $(function () {
         $('.navbar ul li:last-child').html('<a class="btn btn-min-width" href="/investor/dashboard">我的账户</a>');
         $('.navbar ul li:nth-last-child(2)').html('<a class="btn btn-min-width-sm" href="/logout">退出</a>');
         $('.ajax_wait').hide();
-        ajax_ok = true;
+        var is_login = true;
     }
 });
