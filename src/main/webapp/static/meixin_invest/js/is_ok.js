9c26534f38bf27b1dac6a902a85058e47966757c
jingyu.zhang@meixinfinance.com <jingyu.zhang@meixinfinance.com>
2016-08-09 12:58:06 +0800

    接口分离——yellow_stone

--- src/main/webapp/static/meixin_invest/js/is_ok.js
+++ src/main/webapp/static/meixin_invest/js/is_ok.js
@@ -1,8 +1,7 @@
 /**
  * Created by zhangjingyu on 16/8/5.
  */
-var baseUrl='http://101.201.112.171:8082/apiv2';
-var is_success = false, b_is_login=false;
+var is_success = false, b_is_login = false;
 var token = $.cookie('mx_sid');
 
 if (!token) {
@@ -14,111 +13,85 @@ function setHeight() {
     $('.ajax_wait').height($(window).height());
 }
 
+//页面加载完成,get请求
+var getAjaxData = function (options) {
+    $.ajax({
+        type: options.type || 'get',
+        // type: 'get',
+        url: options.url,
+        data: options.data,
+        timeout: 5e3,
+        success: function (data) {
+            // console.log(JSON.stringify(data));
+            var res = data.body || null;
+            if (data && data.code && data.code == -1) {
+                //window.location = 'https://www.meixinfinance.com/login';
+                // options.fn && options.fn(res);
+            }
+
+            if (data && data.code && data.code == 0) {
+                // 提示网络问题
+                $('.ajax_wait_gif img').attr('url', '/dist/meixin_invest/img/net_lazy.png');
+            }
+
+            if (data && data.code && data.code == 1) {
+                options.fn && options.fn(res);
+            }
+        },
+        complete: function (XMLHttpRequest, status) { //请求完成后最终执行参数
+            if (status == 'timeout') {//超时,status还有success,error等值的情况
+                // ajaxTimeoutTest.abort();
+                //提示网络问题
+                $('.ajax_wait_gif img').attr('url', '/dist/meixin_invest/img/net_lazy.png');
+            }
+        }
+    });
+};
+
+
+var baseUrl = 'http://101.201.112.171:8082/apiv2';
+
 $(function () {
     setTimeout(function () {
         $('.ajax_wait p').show();
     }, 3e3);
-    //页面加载完成,get请求
-    var getAjaxData = function (options) {
-        $.ajax({
-            type: 'get',
-            url: options.url,
-            data: options.data,
-            timeout: 5e3,
-            success: function (data) {
-                console.log(JSON.stringify(data));
-                if (data && data.code && data.code == -1) {
-                    //window.location = 'https://www.meixinfinance.com/login';
-                    //options.fn;
-                }
-
-                if (data && data.code && data.code == 0) {
-                    // 提示网络问题
-                    $('.ajax_wait_gif img').attr('url', '/dist/meixin_invest/img/net_lazy.png');
-                }
-
-                if (data && data.code && data.code == 1) {
-                    var d = data.body;
-                    options.fn;
-                }
-            },
-            complete: function (XMLHttpRequest, status) { //请求完成后最终执行参数
-                if (status == 'timeout') {//超时,status还有success,error等值的情况
-                    // ajaxTimeoutTest.abort();
-                    //提示网络问题
-                    $('.ajax_wait_gif img').attr('url', '/dist/meixin_invest/img/net_lazy.png');
-                }
-            }
-        });
-    };
-    var postAjaxData = function (url, data, type) {
-        $.ajax({
-            type: 'post',
-            url: url,
-            data: data,
-            timeout: 5e3,
-            success: function (data) {
-                if (data && data.code && data.code == -1) {
-                    //window.location = 'https://www.meixinfinance.com/login';
-                }
-
-                if (data && data.code && data.code == 0) {
-                    // 提示网络问题
-                }
-
-                if (data && data.code && data.code == 1) {
-                    switch (type) {
-
-                    }
-                }
-            },
-            complete: function (XMLHttpRequest, status) { //请求完成后最终执行参数
-                if (status == 'timeout') {//超时,status还有success,error等值的情况
-                    // ajaxTimeoutTest.abort();
-                    //提示网络问题
-                }
-            }
-        });
-    };
 
     // 调取 登录接口
     getAjaxData({
-        "url":baseUrl+"/auth/is_login",
-        "data":'',
-        "fn":is_login()
+        "url": baseUrl + "/auth/login",
+        "type": "post",
+        "data": {"user_name": "+86 18810797876", "password": "111111"},
+        "fn": is_login
+    });
+    // 验证是否登录
+    getAjaxData({
+        "url": baseUrl + "/auth/is_login",
+        "data": "",
+        "fn": is_login
     });
 
     //is_login  调取登录接口
-    function is_login() {
+    function is_login(res) {
         $('.navbar ul li:last-child').html('<a class="btn btn-min-width" href="/investor/dashboard">我的账户</a>');
         $('.navbar ul li:nth-last-child(2)').html('<a class="btn btn-min-width-sm" href="/logout">退出</a>');
-        b_is_login = true;
         $('.ajax_wait').hide();
+        b_is_login = true;
     }
 
+    if (b_is_login && token) {
+        getAjaxData({
+            "url": baseUrl + "/profile/get",
+            "data": '',
+            "fn": get_profile
+        });
+    }
 
-
-    //get_profile 调取获取信息接口
     function get_profile(d) {
         $('#lastname')[0].value = d.real_name || '';
         $('#firstname')[0].value = d.first_name || '';
         $('#birth-date')[0].value = d.date_of_birth || '';
         $('#birth-countries-states')[0].value = d.country_of_birth || '';
         $('#citizenship-countries-states')[0].value = d.nationality || '';
-        $('#occupation')[0].value = d.occupation || '';/*
-        $('#lastname')[0].value = d.last_name || '';
-        $('#lastname')[0].value = d.last_name || '';
-        $('#lastname')[0].value = d.last_name || '';
-        $('#lastname')[0].value = d.last_name || '';
-        $('#lastname')[0].value = d.last_name || '';
-        $('#lastname')[0].value = d.last_name || '';
-        $('#lastname')[0].value = d.last_name || '';
-        $('#lastname')[0].value = d.last_name || '';
-        $('#lastname')[0].value = d.last_name || '';
-        $('#lastname')[0].value = d.last_name || '';
-        $('#lastname')[0].value = d.last_name || '';
-        $('#lastname')[0].value = d.last_name || '';
-        $('#lastname')[0].value = d.last_name || '';
-        $('#lastname')[0].value = d.last_name || '';*/
+        $('#occupation')[0].value = d.occupation || '';
     }
 });
