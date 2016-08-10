395aae18c49853bcf94f60df355132fc085c8e74
jingyu.zhang@meixinfinance.com <jingyu.zhang@meixinfinance.com>
2016-08-10 12:34:08 +0800

    接口分离——yellow_stone

--- src/main/webapp/static/meixin_invest/js/is_ok.js
+++ src/main/webapp/static/meixin_invest/js/is_ok.js
@@ -1,9 +1,17 @@
 /**
  * Created by zhangjingyu on 16/8/5.
  */
-var is_success = false, b_is_login = false;
-var token = $.cookie('mx_sid');
 
+//获取url中的参数
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+    if (r != null) return unescape(r[2]); return null; //返回参数值
+}
+
+
+var is_success = false, cookie_tooken = {};
+var token = $.cookie('mx_sid');
 if (!token) {
     window.location = 'https://www.meixinfinance.com';
 }
@@ -14,7 +22,7 @@ function setHeight() {
 }
 
 //页面加载完成,get请求
-var getAjaxData = function (options) {
+var Ajax_Data = function (options) {
     $.ajax({
         type: options.type || 'get',
         // type: 'get',
@@ -22,7 +30,7 @@ var getAjaxData = function (options) {
         data: options.data,
         timeout: 5e3,
         success: function (data) {
-            // console.log(JSON.stringify(data));
+            console.log(options.url+": "+JSON.stringify(data));
             var res = data.body || null;
             if (data && data.code && data.code == -1) {
                 //window.location = 'https://www.meixinfinance.com/login';
@@ -49,49 +57,105 @@ var getAjaxData = function (options) {
 };
 
 
-var baseUrl = 'http://101.201.112.171:8082/apiv2';
+var baseUrl = 'http://bj.meixinfinance.com:8082/apiv2';
 
 $(function () {
     setTimeout(function () {
         $('.ajax_wait p').show();
     }, 3e3);
 
+
     // 调取 登录接口
-    getAjaxData({
+    Ajax_Data({
         "url": baseUrl + "/auth/login",
         "type": "post",
         "data": {"user_name": "+86 18810797876", "password": "111111"},
-        "fn": is_login
+        "fn": login
     });
-    // 验证是否登录
-    getAjaxData({
-        "url": baseUrl + "/auth/is_login",
-        "data": "",
-        "fn": is_login
-    });
+
+
+    function login(res) {
+        $('.navbar ul li:last-child').html('<a class="btn btn-min-width" href="/investor/dashboard">我的账户</a>');
+        $('.navbar ul li:nth-last-child(2)').html('<a class="btn btn-min-width-sm" href="/logout">退出</a>');
+        $('.ajax_wait p,.ajax_wait').hide();
+
+        //清楚cookie
+        $.cookie('mx_token', '', {expires: -1});
+        $.cookie('mx_secret', '', {expires: -1});
+        // 设置cookie
+        $.cookie('mx_token', res.mx_token, {expires: 10});
+        $.cookie('mx_secret', res.mx_secret, {expires: 10});
+
+        //取cookie
+        var mx_token = $.cookie('mx_token'),
+            mx_secret = $.cookie('mx_secret');
+
+        cookie_tooken = {mx_token: mx_token, mx_secret: mx_secret};
+
+        // 验证是否登录
+        Ajax_Data({
+            "url": baseUrl + "/auth/is_login",
+            "data": cookie_tooken,
+            "fn": is_login
+        });
+    }
 
     //is_login  调取登录接口
     function is_login(res) {
         $('.navbar ul li:last-child').html('<a class="btn btn-min-width" href="/investor/dashboard">我的账户</a>');
         $('.navbar ul li:nth-last-child(2)').html('<a class="btn btn-min-width-sm" href="/logout">退出</a>');
-        $('.ajax_wait').hide();
-        b_is_login = true;
-    }
+        $('.ajax_wait p,.ajax_wait').hide();
 
-    if (b_is_login && token) {
-        getAjaxData({
+        var get_profile_data = {
+            "fields": ""
+        };
+
+        var object = $.extend({}, cookie_tooken, get_profile_data);
+        var get_profile_data = $.extend({}, get_profile_data, cookie_tooken);
+        Ajax_Data({
             "url": baseUrl + "/profile/get",
-            "data": '',
+            "data": get_profile_data,
             "fn": get_profile
         });
+        $('#submit_step_one').on('click', function () {
+            // 创建订单
+            var create_order_data = {
+                "product_id": 8,
+                "invest_amount": 10000
+            };
+            create_order_data = $.extend({}, create_order_data, cookie_tooken);
+            Ajax_Data({
+                "type": "post",
+                "url": baseUrl + "/order/create",
+                "data": create_order_data,
+                "fn": create_order_res
+            })
+        })
     }
 
+    function create_order_res(res) {
+        alert(res);
+    }
+
+    //获取profile信息
     function get_profile(d) {
-        $('#lastname')[0].value = d.real_name || '';
-        $('#firstname')[0].value = d.first_name || '';
-        $('#birth-date')[0].value = d.date_of_birth || '';
-        $('#birth-countries-states')[0].value = d.country_of_birth || '';
-        $('#citizenship-countries-states')[0].value = d.nationality || '';
-        $('#occupation')[0].value = d.occupation || '';
+        if (d.last_name != "" && d.last_name != null) {
+            $('#lastname').val(d.last_name);
+        }
+        if (d.first_name != "" && d.first_name != null) {
+            $('#firstname').val(d.first_name);
+        }
+        if (d.date_of_birth != "" && d.date_of_birth != null) {
+            $('#birth-date').val(d.date_of_birth);
+        }
+        if (d.country_of_birth != "" && d.country_of_birth != null) {
+            $('#birth-countries-states').val(d.country_of_birth);
+        }
+        if (d.nationality != "" && d.nationality != null) {
+            $('#citizenship-countries-states').val(d.nationality);
+        }
+        if (d.occupation != "" && d.occupation != null) {
+            $('#occupation').val(d.occupation);
+        }
     }
 });
