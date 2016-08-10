7d1465a8c050dddf8ecffe81d0a3a353fdf3f99f
jingyu.zhang@meixinfinance.com <jingyu.zhang@meixinfinance.com>
2016-08-10 14:40:41 +0800

    地推活动

--- src/main/webapp/static/meixin_invest/js/activity-0808_01.js
+++ src/main/webapp/static/meixin_invest/js/activity-0808_01.js
@@ -1,31 +1,28 @@
-$(function(){
+$(function () {
     // cookie数据
     $('#sub').click(function () {
-        var keys=[], values=[];
+        var values = [];
         $('.mian-list').each(function () {
-            var value=$(this).find(".mian-list-select input[type='radio']:checked").val();
-            if(typeof (value)!='undefined'&&value!=''){
-                var key=$(this).find('.mian-list-style p').html();
-            }
-            if(typeof (value)!='undefined'&&value!=''){
+            var value = $(this).find(".mian-list-select input[type='radio']:checked").val();
+            if (typeof (value) != 'undefined' && value != '') {
                 values.push(value);
-            }else{
-
-            }
-            if(typeof (key)!='undefined'&&key!=''){
-                keys.push(key);
             }
         });
-        $.cookie('key_cookie',keys);
-        $.cookie('val_cookie',values);
-        if(
-            (document.getElementById('input1').checked ||document.getElementById('input2').checked)&&
-            (document.getElementById('input3').checked||document.getElementById('input4').checked|| document.getElementById('input5').checked|| document.getElementById('input6').checked)
-            &&(document.getElementById('input7').checked|| document.getElementById('input8').checked|| document.getElementById('input9').checked)
-        ){
+        var key_cookie = {};
+        key_cookie['你是否有离岸银行账户(包含港澳台地区)?'] = values[0]||'';
+        key_cookie['你每年出境旅游或者公差出游的次数是?'] = values[1]||'';
+        key_cookie['你是否有兴趣做海外资产配置?'] = values[2]||'';
+        var jsonStr = JSON.stringify(key_cookie);
+        console.log(jsonStr);
+        $.cookie('key_cookie', jsonStr);
+        if (
+            (document.getElementById('input1').checked || document.getElementById('input2').checked) &&
+            (document.getElementById('input3').checked || document.getElementById('input4').checked || document.getElementById('input5').checked || document.getElementById('input6').checked)
+            && (document.getElementById('input7').checked || document.getElementById('input8').checked || document.getElementById('input9').checked)
+        ) {
             // alert("全都选了");
             window.location = "http://localhost:63342/glacier_static/src/main/webapp/WEB-INF/template/activity/mb_02.html";
-        }else{
+        } else {
             alert("您尚有未选择项");
         }
     });
--- src/main/webapp/static/meixin_invest/js/activity-0808_02.js
+++ src/main/webapp/static/meixin_invest/js/activity-0808_02.js
@@ -1,31 +1,36 @@
-var countdown=6;
-var yzm=$("#yzm");
-yzm.click(function() {
+var countdown = 6;
+var yzm = $("#yzm");
+yzm.click(function () {
     var stop;
-    var countdown=6;
-    function setTime(myVal){
+    var countdown = 6;
+
+    function setTime(myVal) {
         if (countdown == 0) {
-            myVal.prop("disabled",false);
+            myVal.prop("disabled", false);
             myVal.val("获取验证码");
             clearInterval(stop);
-            myVal.css('border','1px solid #0d2a78');
-            countdown=60;
+            myVal.css('border', '1px solid #0d2a78');
+            countdown = 60;
         } else {
-            myVal.css('border',' 0px');
+            myVal.css('border', ' 0px');
             --countdown;
             console.log(countdown);
             myVal.prop("disabled", true);
-            myVal.val("" + countdown +'s'+ "");
+            myVal.val("" + countdown + 's' + "");
         }
     }
-    stop = setInterval(function() {
+
+    stop = setInterval(function () {
         setTime(yzm);
-    },1000)
+    }, 1000)
+    if( countdown==0){
+        clearInterval(stop);
+    }
 });
 
 $(function () {
     $('#yzm').click(function () {
-        var data={"phone":"+86 " + $('#phone').val()};
+        var data = {"phone": "+86 " + $('#phone').val()};
         $.ajax({
             type: "get",
             url: "http://101.201.112.171/sendVerifyCode",
@@ -37,36 +42,22 @@ $(function () {
     });
 
     $('#signUp').click(function () {
-        var keys=$.cookie("key_cookie");
-        var vals=$.cookie("val_cookie");
-        keys=keys.split(',');
-        vals=vals.split(',');
-        var keys0=keys[0];
-        var val0=vals[0];
-        var keys1=keys[1];
-        var val1=vals[1];
-        var keys2=keys[2];
-        var val2=vals[2];
-        var data={
-            "phone":"+86 "+$('#phone').val(),
-            "verifyCode":$("#id_verify").val(),
-            "password":$("#pwd").val(),
-            "email":$("#email").val(),
-            "questions":{
-                keys0:val0,
-                keys1:val1,
-                keys2:val2
-            }
+        var jsonStr = $.cookie("key_cookie");
+        var data = {
+            "phone": "+86 " + $('#phone').val(),
+            "verifyCode": $("#id_verify").val(),
+            "password": $("#pwd").val(),
+            "email": $("#email").val(),
+            "questions": jsonStr
         };
-        console.log(data);
         $.ajax({
-            type:'post',
+            type: 'post',
             url: "http://101.201.112.171/simple_signup",
-            data:data,
-            success:function (res) {
-                if(res.code==1){
+            data: data,
+            success: function (res) {
+                if (res.code == 1) {
                     console.log(res.msg);
-                }else{
+                } else {
 
                 }
             }
