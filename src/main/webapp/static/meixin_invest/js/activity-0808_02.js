f5e41c018f6e3de2014291f2e3377347bb1d93c0
jingyu.zhang@meixinfinance.com <jingyu.zhang@meixinfinance.com>
2016-08-11 19:15:13 +0800

    地推活动

--- src/main/webapp/static/meixin_invest/js/activity-0808_02.js
+++ src/main/webapp/static/meixin_invest/js/activity-0808_02.js
@@ -59,8 +59,8 @@ $(function () {
                                     if (res.code == 1) {
                                         $("form").remove();
                                         $("div").remove(".bg-logo");
-                                        $('.mian').append("<p style='font-size: 18px; color: #0d2a78; letter-spacing: 1px; text-align: center; padding-top: 230px; line-height: 2em;'>" + '恭喜您' + res.msg + "<br>" + '您的抽奖码:' + res.no + "</p>"
-                                            + "<p style='font-size: 15px;color: #666; line-height: 20px; text-align: center; padding: 0 50px;'>" + '请您将此页面展示给我们的工作人员,即可抽取美元奖励!' + "</p>");
+                                        $('.mian').append("<p style='font-size: 18px; color: #0d2a78; letter-spacing: 1px; text-align: center; margin: 0 auto; padding-top: 230px; line-height: 2em;'>" + '恭喜您' + res.msg + "<br>" + '您的抽奖码:' + res.no + "</p>"
+                                            + "<p style='font-size: 15px;color: #666; margin: 0 auto; line-height: 20px; text-align: center; padding: 0 50px;'>" + '请您将此页面展示给我们的工作人员,即可抽取美元奖励!' + "</p>");
                                     } else if (res.code = -1) {
                                        /* $('.mian').append("<p style='font-size: 15px;color: #666; line-height: 20px;" +
                                             "text-align: center; padding: 0 50px'>" + res.msg + "</p>");*/
