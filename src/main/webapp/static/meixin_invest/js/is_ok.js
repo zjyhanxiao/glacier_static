de5e9c00e28f621644844b8873327afe34e4b6bf
jingyu.zhang@meixinfinance.com <jingyu.zhang@meixinfinance.com>
2016-08-15 18:20:36 +0800

    接口分离 yellow_stone

--- src/main/webapp/static/meixin_invest/js/is_ok.js
+++ src/main/webapp/static/meixin_invest/js/is_ok.js
@@ -1,7 +1,6 @@
 /**
  * Created by zhangjingyu on 16/8/5.
  */
-
 //获取url中的参数
 function getUrlParam(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
@@ -10,6 +9,8 @@ function getUrlParam(name) {
     return null; //返回参数值
 }
 
+var address_proof;//上传护照
+var order_number;//订单order_number;
 
 var is_new = true, is_old = false;
 
@@ -31,17 +32,20 @@ var Ajax_Data = function (options) {
         url: options.url,
         data: options.data,
         timeout: 5e3,
-        async:options.async,
+        async: options.async,
         success: function (data) {
-            // console.log(options.url + ": " + JSON.stringify(data));
+            console.log(options.url + ": " + JSON.stringify(data));
             var res = data.body || null;
-            if (data && data.code && data.code == -1) {
-                options.fail_fn && options.fail_fn(res);
+            var login_fail_data = data.msg || null;
+            console.log(login_fail_data);
+            console.log(data.code);
+            if (data.code == -1) {
+                options.no_login && options.no_login(res);
             }
 
-            if (data && data.code && data.code == 0) {
+            if (data.code == 0) {
                 // 提示网络问题
-                $('.ajax_wait_gif img').attr('url', '/dist/meixin_invest/img/net_lazy.png');
+                options.fail_fn && options.fail_fn(login_fail_data);
             }
 
             if (data && data.code && data.code == 1) {
@@ -57,9 +61,28 @@ var Ajax_Data = function (options) {
         }
     });
 };
+//创建订单失败执行
+function create_order_fail(d){
+    $('.ajax_wait p,.ajax_wait').hide();
+    alert(d);
+}
 
-
-var baseUrl = 'http://bj.meixinfinance.com:8081/web';
+//更新profile信息失败执行
+function update_profile_fail(d){
+    $('.ajax_wait p,.ajax_wait').hide();
+    alert(d);
+}
+//提交订单失败执行
+function commit_order_fail(d){
+    $('.ajax_wait p,.ajax_wait').hide();
+    alert(d);
+}
+//更新bank信息失败执行
+function update_bank_fail(d){
+    $('.ajax_wait p,.ajax_wait').hide();
+    alert(d);
+}
+var baseUrl = 'http://192.168.1.100:8081/web';
 // var baseUrl = 'http://101.201.112.171:8082/web';
 
 $(function () {
@@ -70,14 +93,19 @@ $(function () {
         $('section input').eq(index).attr('data-parsley-group', 'block-' + index);
     });
     // 调取 登录接口
-    /*Ajax_Data({
+/*    Ajax_Data({
         "url": baseUrl + "/auth/login",
         "type": "post",
-        // "data": {"user_name": "+86 15001393659", "password": "123456"},
-        "data": {"user_name": "+86 18810797876", "password": "111111"},
-        "fn": login
+        "data": {"user_name": "+86 15001393659", "password": "123456789"},
+        // "data": {"user_name": "+86 18810797876", "password": "111111"},
+        "fn": login,
+        "fail_fn": login_fail
     });
 
+    function login_fail(d) {
+        $('.ajax_wait p,.ajax_wait').hide();
+        alert(d);
+    }
 
     function login(res) {
         $('.navbar ul li:last-child').html('<a class="btn btn-min-width" href="/investor/dashboard">我的账户</a>');
