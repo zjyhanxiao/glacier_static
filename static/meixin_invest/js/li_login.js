1af06489690ed603841ee6e1d70d5d44541be23c
lizhiqiang <lixiaofan0127@163.com>
2016-08-23 16:24:11 +0800

    登录 注册

--- static/meixin_invest/js/li_login.js
+++ static/meixin_invest/js/li_login.js
@@ -2,50 +2,63 @@
  * Created by admin on 2016/8/22.
  */
 $(function () {
-    //手机号登录
-    if ($('#reset_type_phone').hasClass('active')){
-        $('#login').click(function () {
-            var data = {};
+    $("#login").on('click',function() {
+        var data = {};
+        if($('#general').hasClass('active')){
             data.user_name = $('#id_telephone').val();
             data.password = $('#password').val();
-            console.log(data);
-            $.ajax({
-                type: 'post',
-                url: "http://101.201.112.171:8082/web/auth/login",
-                data: data,
-                success: function (res) {
-                    if (res.code == 1) {
-                        alert(res.msg);
-                    } else if (res.code = -1) {
-                        alert(res.msg);
+            if ($('#id_telephone').val().length<6) {
+                alert("请输入手机号");
+                return false;
+            }
+            else if ($('input[type="password"]').val() == '') {
+                alert("请输入密码");
+                return false;
+            }
+            else {
+                $.ajax({
+                    type: 'post',
+                    url: "http://101.201.112.171:8082/web/auth/login",
+                    data: data,
+                    success: function (res) {
+                        if (res.code == 1) {
+                            alert(res.msg);
+                        } else if (res.code == -1) {
+                            alert(res.msg);
+                        }
                     }
-                }
-            })
-        });
-    }
-    if($('#reset_type_email').hasClass('active')){
-        $('#login').click(function () {
-            alert($('#email').val());
-            var data = {};
-            data.user_name = $('#email').val();
-            data.password = $('#password').val();
-            console.log(data);
-            $.ajax({
-                type: 'post',
-                url: "http://101.201.112.171:8082/web/auth/login",
-                data: data,
-                success: function (res) {
-                    if (res.code == 1) {
-                        alert(res.msg);
-                    } else if (res.code = -1) {
-                        alert(res.msg);
+                })
+            }
+        }
+
+        var data1 = {};
+        if($('#invest').hasClass('active')){
+            data1.user_name = $('#email').val();
+            data1.password = $('#password').val();
+            if ($('#email').val().length<6) {
+                alert("请输入手机号");
+                return false;
+            }
+            else if ($('input[type="password"]').val() == '') {
+                alert("请输入密码");
+                return false;
+            }
+            else {
+                $.ajax({
+                    type: 'post',
+                    url: "http://101.201.112.171:8082/web/auth/login",
+                    data: data1,
+                    success: function (res) {
+                        if (res.code == 1) {
+                            alert(res.msg);
+                        } else if (res.code == -1) {
+                            alert(res.msg);
+                        }
                     }
-                }
-            })
-        });
-    }
-
-
-
+                })
+            }
+        }
+        return false;
+    });
 
 });
