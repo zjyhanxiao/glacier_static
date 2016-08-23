18b546071bda703b1b9dad8919374baed602133e
lizhiqiang <lixiaofan0127@163.com>
2016-08-23 14:09:20 +0800

    登陆 注册

--- static/meixin_invest/js/li_login.js
+++ static/meixin_invest/js/li_login.js
@@ -2,30 +2,50 @@
  * Created by admin on 2016/8/22.
  */
 $(function () {
-    //调用登陆接口
-    $('#login').click(function () {
-        var data = {
-            "phone": $('#id_telephone').val(),
-            "email":$("#email").val(),
-            "password":$("#password").val()
-        };
-        if(data.length>0){
+    //手机号登录
+    if ($('#reset_type_phone').hasClass('active')){
+        $('#login').click(function () {
+            var data = {};
+            data.user_name = $('#id_telephone').val();
+            data.password = $('#password').val();
+            console.log(data);
             $.ajax({
                 type: 'post',
-                url: "/auth/login",
+                url: "http://101.201.112.171:8082/web/auth/login",
                 data: data,
                 success: function (res) {
                     if (res.code == 1) {
-                        alert('登陆成功');
+                        alert(res.msg);
                     } else if (res.code = -1) {
-                        alert('登录失败');
+                        alert(res.msg);
                     }
                 }
             })
-        }
+        });
+    }
+    if($('#reset_type_email').hasClass('active')){
+        $('#login').click(function () {
+            alert($('#email').val());
+            var data = {};
+            data.user_name = $('#email').val();
+            data.password = $('#password').val();
+            console.log(data);
+            $.ajax({
+                type: 'post',
+                url: "http://101.201.112.171:8082/web/auth/login",
+                data: data,
+                success: function (res) {
+                    if (res.code == 1) {
+                        alert(res.msg);
+                    } else if (res.code = -1) {
+                        alert(res.msg);
+                    }
+                }
+            })
+        });
+    }
 
 
-    });
 
 
 });
