58f4cbd4afdff3e0fb1d1fb8b3fb89e93cd6e960
lizhiqiang <lixiaofan0127@163.com>
2016-09-08 14:50:43 +0800

    基金_列表

--- static/version_1.0/meixin_invest/js/activity-0808_02.js
+++ static/version_1.0/meixin_invest/js/activity-0808_02.js
@@ -66,13 +66,14 @@ $(function () {
                 data: data,
                 success: function (res) {
                     if (res.code == 1) {
-                        var mx = res.body;
                         $("form").remove();
                         $("div").remove(".bg-logo");
                         $('.mian').append("<p style='font-size: 18px; color: #0d2a78; letter-spacing: 1px; text-align: center; margin: 0 auto; padding-top: 230px; line-height: 2em;'>" + '恭喜您' + res.msg + "<br>" + '您的抽奖码:' + res.no + "</p>"
-                            + "<p style='font-size: 15px;color: #666; margin: 0 auto; line-height: 20px; text-align: center; padding: 0 50px;'>" + '请您将此页面展示给我们的工作人员,即可抽取美元奖励!' + "</p>");
+                            + "<p style='font-size: 15px;color: #666; margin: 0 auto; line-height: 20px; text-align: center; padding: 0 50px;'>" + '请您将此页面展示给我们的工作人员,即可抽取美元奖励!' + "</p>"
+                            + "<p id='time'>" + + "</p>" );
                     } else if (res.code = -1) {
                         $('#infor').html(res.msg);
+                        window.location.href = 'https://s1.meixinglobal.com/web/activity/lose_track.html';
                     }
                 }
             })
