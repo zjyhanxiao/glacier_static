240232d8d64efea65b3221f4de606846fcf1ad96
lizhiqiang <lixiaofan0127@163.com>
2016-09-08 16:22:55 +0800

    地推

--- static/version_1.0/meixin_invest/js/activity-0808_02.js
+++ static/version_1.0/meixin_invest/js/activity-0808_02.js
@@ -1,6 +1,7 @@
 var b_checkPhone = false, b_checkPwd = false, b_checkEmail = false, b_verify_code = false;
 
-console.log(b_checkPhone);
+//console.log(b_checkPhone);
+
 $(function () {
     $('#yzm').click(function () {
         var data = {"phone": "+86 " + $('#phone').val()};
@@ -43,6 +44,10 @@ $(function () {
     });
 
     $('#signUp').click(function () {
+        
+       /* function doReload () {
+            window.location.href = 'https://www.meixinfinance.com/';
+        }*/
         $('p').remove();
         checkPhone() && checkPwd() && checkEmail();
         var jsonStr = $.cookie("key_cookie");
@@ -70,10 +75,11 @@ $(function () {
                         $("div").remove(".bg-logo");
                         $('.mian').append("<p style='font-size: 18px; color: #0d2a78; letter-spacing: 1px; text-align: center; margin: 0 auto; padding-top: 230px; line-height: 2em;'>" + '恭喜您' + res.msg + "<br>" + '您的抽奖码:' + res.no + "</p>"
                             + "<p style='font-size: 15px;color: #666; margin: 0 auto; line-height: 20px; text-align: center; padding: 0 50px;'>" + '请您将此页面展示给我们的工作人员,即可抽取美元奖励!' + "</p>"
-                            + "<p id='time'>" + + "</p>" );
+                            + "<p>"+ '5s后自动跳转到官网首页' +"</p>");
+                        setTimeout(function (){  window.location.href = 'https://www.meixinfinance.com/'; },5000);
                     } else if (res.code = -1) {
                         $('#infor').html(res.msg);
-                        window.location.href = 'https://s1.meixinglobal.com/web/activity/lose_track.html';
+                        // window.location.href = 'https://www.meixinfinance.com/web/activity/lose_track.html';
                     }
                 }
             })
