cf6343291fbe2b86645bb553758adb9768aa6331
lizhiqiang <lixiaofan0127@163.com>
2016-09-08 22:25:26 +0800

    登录

--- static/version_1.1/meixin_invest/js/login.js
+++ static/version_1.1/meixin_invest/js/login.js
@@ -14,22 +14,25 @@ $(function () {
                 $("#id_telephone").css("border-color","#ff0000");
                 return false;
             }
-            else if ($('input[type="password"]').val() == '') {
+            if ($('input[type="password"]').val() == '') {
                 $("#password").css("border-color","#ff0000");
                 return false;
             }
             else {
+                $("#login").prop('disabled',true);
                 $.ajax({
                     type: 'post',
                     url: baseUrl + "/auth/login",
                     data: data,
                     success: function (res) {
                         if (res.code == 1) {
+                            $("#login").prop('disabled',false);
                             var mx = res.body;
                             $.cookie('mx_token', mx.mx_token, {expires: 30});
                             $.cookie('mx_secret', mx.mx_secret, {expires: 30});
                             window.location.href = '/';
                         } else if (res.code != 1) {
+                            $("#login").prop('disabled',false);
                             $("#error-place").html("<div style='text-align: center;' class='alert alert-warning'>" + res.msg +"</div>");
                         }
                     }
@@ -47,22 +50,25 @@ $(function () {
                 $("#email").css("border-color","#ff0000");
                 return false;
             }
-            else if ($('input[type="password"]').val() == '') {
+            if ($('input[type="password"]').val() == '') {
                 $("#password").css("border-color","#ff0000");
                 return false;
             }
             else {
+                $("#login").prop('disabled',true);
                 $.ajax({
                     type: 'post',
                     url: baseUrl + "/auth/login",
                     data: data1,
                     success: function (res) {
                         if (res.code == 1) {
+                            $("#login").prop('disabled',false);
                             var mx = res.body;
                             $.cookie('mx_token', mx.mx_token, {expires: 30});
                             $.cookie('mx_secret', mx.mx_secret, {expires: 30});
                             window.location.href = '/';
                         } else if (res.code != 1) {
+                            $("#login").prop('disabled',false);
                             $("#error-place").html("<div style='text-align: center;' class='alert alert-warning'>" + res.msg + "</div>");
                         }
                     }
